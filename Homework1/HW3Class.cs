901a6f63e4dcedf0b638504dcb9a41407c827d2c
Lev <leva.100s@gmail.com>
2022-03-06 23:54:17 +0300

    Add Task 1, HW3

---
+++ Homework1/HW3Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    internal class HW3Class
+    {
+        public static int RaiseTheNumberToTheDegreeofTheSecondNumber(int a, int b)
+        {
+            int result = 1;
+            for (int i = 0; i < b; i++)
+            {
+                result *= a;
+            }
+            return result;
+        }
+
+
+
+
+
+    }
+}
